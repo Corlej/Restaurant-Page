2b3e2bb4e081a03639fef7036981f8d96f748013
Jack <Jcorle3@gmail.com>
2023-06-17 14:22:36 +0900

    first commit with initial webpack setup, add menu navigation

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('.header2');
+    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+  
+    if (scrollPosition > viewportHeight) {
+      header.classList.add('visible');
+    } else {
+      header.classList.remove('visible');
+    }
+  });
