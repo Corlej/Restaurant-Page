ff5bbed817000d132adec20bb5a0a5f152a3df6d
Jack <Jcorle3@gmail.com>
2023-06-18 13:59:16 +0900

    first commit, initial layout for header and webpack

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './style.css';
-import './logo.svg';
+
 window.addEventListener('scroll', function() {
     var header = document.querySelector('.header2');
-    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
     var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
   
     if (scrollPosition > viewportHeight) {
